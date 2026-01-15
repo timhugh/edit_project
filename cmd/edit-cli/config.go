34c11b4fb19fa4c0159093121d4c4ff42d77c171
Tim Heuett <tim.heuett@gmail.com>
2026-01-15 07:11:37 -0800

    update config commands to work with new structure

--- cmd/edit-cli/config.go
+++ cmd/edit-cli/config.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"os"
+	"syscall"
 
+	"github.com/hairyhenderson/go-which"
 	"github.com/spf13/cobra"
 	"github.com/timhugh/edit_project"
 )
@@ -12,6 +15,51 @@ import (
 var configCmd = &cobra.Command{
 	Use:   "config",
 	Short: "Interact with tool configuration",
+}
+
+var configCreateCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Write current configuration to file",
+	Run: func(cmd *cobra.Command, args []string) {
+		// We don't care if this fails because there probably isn't a config file yet
+		// and we'll at least get the default values
+		config, _ := edit_project.LoadConfig(configPath)
+
+		if err := edit_project.SaveConfig(configPath, &config); err != nil {
+			panic(err)
+		}
+
+		jsonOutput, err := json.MarshalIndent(config, "", "  ")
+		if err != nil {
+			panic(err)
+		}
+		cmd.Println("Configuration written to", configPath)
+		cmd.Println(string(jsonOutput))
+	},
+}
+
+var configEditCmd = &cobra.Command{
+	Use:   "edit",
+	Short: "Edit the configuration file in your default editor",
+	Run: func(cmd *cobra.Command, args []string) {
+		config, err := edit_project.LoadConfig(configPath)
+		if err != nil {
+			if !errors.Is(err, os.ErrNotExist) {
+				panic(err)
+			}
+			cmd.Println("Configuration file does not exist; creating with default values.")
+			if err := edit_project.SaveConfig(configPath, &config); err != nil {
+				panic(err)
+			}
+		}
+		pathToEditor := which.Which(config.Editor)
+		command := []string{config.Editor, configPath}
+		env := os.Environ()
+		cmd.Println("Opening configuration file in editor:", pathToEditor)
+		if err := syscall.Exec(pathToEditor, command, env); err != nil {
+			panic(err)
+		}
+	},
 }
 
 var configPathCmd = &cobra.Command{
@@ -22,15 +70,38 @@ var configPathCmd = &cobra.Command{
 	},
 }
 
+var configResetCmd = &cobra.Command{
+	Use:   "reset",
+	Short: "Reset the configuration file to default values",
+	Run: func(cmd *cobra.Command, args []string) {
+		defaultConfig := edit_project.DefaultConfig()
+		jsonOutput, err := json.MarshalIndent(defaultConfig, "", "  ")
+		if err != nil {
+			panic(err)
+		}
+		cmd.Printf("Configuration file %s will be reset to default values\n:", configPath)
+		cmd.Println(string(jsonOutput))
+		cmd.Printf("Continue? (y/N): ")
+		var response string
+		_, err = fmt.Scanln(&response)
+		if err != nil || (response != "y" && response != "Y") {
+			cmd.Println("Aborting.")
+			return
+		}
+		if err := edit_project.SaveConfig(configPath, &defaultConfig); err != nil {
+			panic(err)
+		}
+		cmd.Println("Configuration reset")
+	},
+}
+
 var configShowCmd = &cobra.Command{
 	Use:   "show",
 	Short: "Show the current configuration",
 	Run: func(cmd *cobra.Command, args []string) {
 		config, err := edit_project.LoadConfig(configPath)
-		if err != nil {
-			if !errors.Is(err, os.ErrNotExist) {
-				panic(err)
-			}
+		if err != nil && !errors.Is(err, os.ErrNotExist) {
+			panic(err)
 		}
 		jsonOutput, err := json.MarshalIndent(config, "", "  ")
 		if err != nil {
@@ -40,23 +111,11 @@ var configShowCmd = &cobra.Command{
 	},
 }
 
-var configCreateCmd = &cobra.Command{
-	Use:   "create",
-	Short: "Create a configuration file (will prompt for values)",
-	Run: func(cmd *cobra.Command, args []string) {
-		// pulling current config values to use as defaults,
-		// but we don't care if it fails
-		config, _ := edit_project.LoadConfig(configPath)
-
-		if err := edit_project.SaveConfig(configPath, &config); err != nil {
-			panic(err)
-		}
-	},
-}
-
 func init() {
 	rootCmd.AddCommand(configCmd)
+	configCmd.AddCommand(configCreateCmd)
+	configCmd.AddCommand(configEditCmd)
 	configCmd.AddCommand(configPathCmd)
 	configCmd.AddCommand(configShowCmd)
-	configCmd.AddCommand(configCreateCmd)
+	configCmd.AddCommand(configResetCmd)
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require github.com/spf13/cobra v1.10.2
 
 require (
 	github.com/go-test/deep v1.1.1 // indirect
+	github.com/hairyhenderson/go-which v0.2.2 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
 )
