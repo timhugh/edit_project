9a2ee929dfca50be0c4a3fdb1c4be5592cb242f9
Tim Heuett <tim.heuett@gmail.com>
2026-01-15 16:41:26 -0800

    add shell functions

--- install.zsh
+++ install.zsh
@@ -1,5 +1,10 @@
 open_project() {
+	local project_dir="$(edit-cli open $1)"
+	cd "$project_dir" || return 1
 }
 
 edit_project() {
+	open_project "$1"
+	local editor=$(edit-cli path-to-editor)
+	"$editor" .
 }
