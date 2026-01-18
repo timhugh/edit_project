81244fbf2d18c1394abd54670038daf68c113ac0
Tim Heuett <tim.heuett@gmail.com>
2026-01-17 17:29:17 -0800

    change workflow to use ci scripts

--- ci/package.sh
+++ ci/package.sh
@@ -6,22 +6,22 @@ project_root=$(git rev-parse --show-toplevel)
 target=$1
 
 local_os=$(uname | tr '[:upper:]' '[:lower:]')
-GOOS=${GOOS:-$local_os}
+OS=${OS:-$local_os}
 case $(uname -m) in
     x86_64) local_arch="amd64" ;;
     aarch64 | arm64) local_arch="arm64" ;;
     *) echo "Unsupported architecture: $(uname -m)" ; exit 1 ;;
 esac
-GOARCH=${GOARCH:-$local_arch}
+ARCH=${ARCH:-$local_arch}
 
-echo "Building target '$target' for $GOOS/$GOARCH"
+echo "Building target '$target' for $OS/$ARCH"
 target_name="${target##*/}"
 cd "$project_root"
 mkdir -p dist
-CGO_ENABLED=0 GOOS="$GOOS" GOARCH="$GOARCH" go build -o "dist/${target_name}" "${target}"
+CGO_ENABLED=0 GOOS="$OS" GOARCH="$ARCH" go build -o "dist/${target_name}" "${target}"
 
 PACKAGE_FORMAT="${PACKAGE_FORMAT:-tar.gz}"
-package_name="${target_name}-${GOOS}-${GOARCH}.${PACKAGE_FORMAT}"
+package_name="${target_name}-${OS}-${ARCH}.${PACKAGE_FORMAT}"
 cd dist
 if [ "$PACKAGE_FORMAT" == "tar.gz" ]; then
     tar -czf "$package_name" "$target_name"
--- ci/test.sh
+++ ci/test.sh
@@ -3,10 +3,12 @@
 set -euo pipefail
 
 project_root=$(git rev-parse --show-toplevel)
-
 cd "$project_root"
 
+echo "Preparing test environment..."
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+
+echo "Running linters and tests..."
 golangci-lint run
 go test ./...
-
 echo "All tests passed!"
