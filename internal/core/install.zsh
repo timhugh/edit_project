93896294abb7a7a7d86114bb0f3a219a1773c473
Tim Heuett <tim.heuett@gmail.com>
2026-01-19 18:11:26 -0800

    don't open the editor if user cancels fzf

--- internal/core/install.zsh
+++ internal/core/install.zsh
@@ -1,10 +1,13 @@
 open_project() {
 	local project_dir="$(edit-cli projects search $1)" || return 1
+    [[ -z "$project_dir" ]] && return 1
 	cd "$project_dir" || return 1
 }
 
 edit_project() {
-	open_project "$1"
+	local project_dir="$(edit-cli projects search $1)" || return 1
+    [[ -z "$project_dir" ]] && return 1
+	cd "$project_dir" || return 1
 	local editor="$(edit-cli config editor-path)" || return 1
 	"$editor" .
 }
